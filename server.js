a9397ee11cb87ce1adfcdbe3e1125c6f828bbcdd
florianGierlichs <gierlichs@d-sire.com>
2020-11-25 11:15:53 +0100

    Add typescript

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require("./lib/routes");
 const { ensureUniqueIdIndex } = require("./lib/shorties");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3003;
 
 app.use(express.json());
 app.use(routes);
